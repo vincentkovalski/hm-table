cfd957511a24ece1d1a0641b3912bc0accdb76c4
vincentkovalski <kovall89@gmail.com>
2022-06-11 16:27:59 +0500

    registred cell

--- hm-table/ViewController.swift
+++ hm-table/ViewController.swift
@@ -19,22 +19,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     private let tableView: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
 
         return table
     }()
 
+    var models = [SettingsOption]()
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        confifure()
+        title = "Настройки"
+
+        view.addSubview(tableView)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
 
 
+    func confifure() {
+        self.models.append(SettingsOption(title: "Авиарежим", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemOrange, handler: {
+            print("Вы нажали на авиарежим")
+        }))
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return models.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.title
+        return cell
     }
 
 
