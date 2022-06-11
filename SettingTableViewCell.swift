b85cf92b5b9a6982a26a908ac6d708bb8895f886
vincentkovalski <kovall89@gmail.com>
2022-06-11 17:36:32 +0500

    add setting View Cell

---
+++ SettingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingTableViewCell.swift
+//  hm-table
+//
+//  Created by Виктор Ковалевский on 11.06.2022.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+
+
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+
+}
