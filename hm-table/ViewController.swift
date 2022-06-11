c66b928a2602a0b94c9fe01aad2dbe7bf16b1f39
vincentkovalski <kovall89@gmail.com>
2022-06-11 15:40:05 +0500

    add struct Settings Option and table View

--- hm-table/ViewController.swift
+++ hm-table/ViewController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+
 
 
 }
