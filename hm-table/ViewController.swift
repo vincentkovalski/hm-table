309af5bb2166c951475231f8d5929104ea34ac61
vincentkovalski <kovall89@gmail.com>
2022-06-11 15:14:10 +0500

    Initial Commit

---
+++ hm-table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hm-table
+//
+//  Created by Виктор Ковалевский on 11.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
