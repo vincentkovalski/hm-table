e81a4ad16a62dcfe2035bad2231743041d1a11b7
vincentkovalski <kovall89@gmail.com>
2022-06-11 17:44:10 +0500

    create property iconContainer, iconImageView, label

--- SettingTableViewCell.swift
+++ SettingTableViewCell.swift
@@ -10,8 +10,29 @@ import UIKit
 class SettingTableViewCell: UITableViewCell {
 
 
+    private let iconContainer: UIView {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = true
+        return view
+    }()
 
-    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .white
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private let label: UILabel = {
+        let label = UILabel()
+        // одна стройка метки
+        label.numberOfLines = 1
+        return label
+    }()
+
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
