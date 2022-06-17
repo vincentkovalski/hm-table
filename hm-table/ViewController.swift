3e760690c59349426e131cc381706d5bb8a685c8
vincentkovalski <kovall89@gmail.com>
2022-06-17 18:22:56 +0500

    vot finish

--- hm-table/ViewController.swift
+++ hm-table/ViewController.swift
@@ -1,23 +1,13 @@
-//
-//  ViewController.swift
-//  hm-table
-//
-//  Created by Виктор Ковалевский on 11.06.2022.
-//
-
 import UIKit
-
 struct Section {
     let options: [SettingsOptionType]
 }
-
 enum SettingsOptionType {
     case staticCell(model: SettingsOption)
     case switchCell(model: SettingsSwitchOption)
     case addLabelCell(model: SettingsAdditionalLabelOption)
     case notificationCell(model: SettingsNotificationOption)
 }
-
 struct SettingsNotificationOption {
     let title: String
     let icon: UIImage?
@@ -26,7 +16,6 @@ struct SettingsNotificationOption {
     let handler: (() -> Void)
     let labelContainerBackgroundColor: UIColor
 }
-
 struct SettingsAdditionalLabelOption {
     let title: String
     let icon: UIImage?
@@ -34,15 +23,12 @@ struct SettingsAdditionalLabelOption {
     let addLabelText: String
     let handler: (() -> Void)
 }
-
-
 struct SettingsOption {
     let title: String
     let icon: UIImage?
     let iconBackgroundColor: UIColor
     let handler: (() -> Void)
 }
-
 struct SettingsSwitchOption {
     let title: String
     let icon: UIImage?
@@ -51,38 +37,25 @@ struct SettingsSwitchOption {
     // дополнительное свойство
     var isOn: Bool
 }
-
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
     private let tableView: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifier)
         table.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifier)
         table.register(AdditionalLabelTableViewCell.self, forCellReuseIdentifier: AdditionalLabelTableViewCell.identifier)
         table.register(NotificationTableViewCell.self, forCellReuseIdentifier: NotificationTableViewCell.identifier)
-
-
         return table
     }()
-
     var models = [Section]()
-
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
         confifure()
         title = "Настройки"
-    
-
         view.addSubview(tableView)
-
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
     }
-
-
     func confifure() {
         models.append((Section(options: [
             SettingsOptionType.switchCell(model: SettingsSwitchOption(title: "Авиарежим", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemOrange, handler: {
@@ -104,8 +77,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     }, isOn: false))
         ]))
         )
-
-
         models.append((Section(options: [
             SettingsOptionType.staticCell(model: SettingsOption(title: "Уведомления", icon: UIImage(named: "icons8-notification-center-100")?.withTintColor(UIColor.white), iconBackgroundColor: .systemRed) {
                 print("Нажата ячейка Уведомления")
@@ -121,7 +92,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }),
         ]))
         )
-
         models.append((Section(options: [
             SettingsOptionType.notificationCell(model: SettingsNotificationOption(title: "Основные", icon: UIImage(systemName: "gear"), iconBackgroundColor: .systemGray, addLabelText: "1", handler: {
                 print("Нажата ячейка Основные")
@@ -138,24 +108,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }),
         ]))
         )
-
     }
-
-
-
-
     func numberOfSections(in tableView: UITableView) -> Int {
         return models.count
     }
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models[section].options.count
     }
-
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let model = models[indexPath.section].options[indexPath.row]
             switch model.self {
-
             case .staticCell(let model):
                 guard let cell = tableView.dequeueReusableCell(
                     withIdentifier: SettingTableViewCell.identifier,
@@ -165,7 +127,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 }
                 cell.configure(with: model)
                 return cell
-
             case .switchCell(let model):
                 guard let cell = tableView.dequeueReusableCell(
                     withIdentifier: SwitchTableViewCell.identifier,
@@ -175,7 +136,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 }
                 cell.configure(with: model)
                 return cell
-
             case .addLabelCell(model: let model):
                 guard let cell = tableView.dequeueReusableCell(
                     withIdentifier: AdditionalLabelTableViewCell.identifier,
@@ -211,9 +171,5 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             model.handler()
         }
     }
-    
-
-
-
 }
 
